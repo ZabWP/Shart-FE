a7e08384fe3f33a3b1a6c2037b7ff78042d7f2eb
ZabWP <zabdiel.bronola01@gmail.com>
2025-04-22 19:22:15 -0400

    making zustand state persist

--- src/stateManagement/userInfoStore.js
+++ src/stateManagement/userInfoStore.js
@@ -1,11 +1,19 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useUserStore = create((set) => ({
-  user: null,
-  username: null,
-  setUser: (userData) => set({ user: userData }),
-  setUsername: (Dusername) => set({ username: Dusername }),
-  logout: () => set({ user: null, username: null }),
-}));
+const useUserStore = create(
+  persist(
+    (set) => ({
+      user: null,
+      username: null,
+      setUser: (userData) => set({ user: userData }),
+      setUsername: (Dusername) => set({ username: Dusername }),
+      logout: () => set({ user: null, username: null }),
+    }),
+    {
+      name: "user-store",
+    }
+  )
+);
 
 export default useUserStore;
