7e508ac07ffae5e375ae204aaeaddfd1439f9a36
ZabWP <zabdiel.bronola01@gmail.com>
2025-04-23 13:46:59 -0400

    my likes component displays the posts the user has liked

--- src/Components/myLikes.jsx
+++ src/Components/myLikes.jsx
@@ -1,8 +1,44 @@
+import { useEffect, useState } from "react";
+import useUserStore from "../stateManagement/userInfoStore";
+import "./myLikes.css";
+
 const MyLikes = () => {
+  const [myLikes, setMyLikes] = useState([]);
+  const { userID } = useUserStore();
+
+  useEffect(() => {
+    const fetchLikes = async () => {
+      try {
+        const res = await fetch(
+          `https://codd.cs.gsu.edu/~zbronola1/SoftwareEngineering/shart/myLikes.php?userID=${userID}`
+        );
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
+        }
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error); // Handle the error returned from PHP
+        }
+
+        setMyLikes(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchLikes();
+  }, [userID]);
+
   return (
     <div>
       <h1>My Likes</h1>
-      <p>This is the My Likes component.</p>
+      <div className="likesContainer">
+        {myLikes.map((like, index) => (
+          <div key={index} className="myLikedItem">
+            <img src={like.artImgLink} alt="pic" />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- src/Components/myProfile.jsx
+++ src/Components/myProfile.jsx
@@ -1,61 +1,67 @@
 import { useEffect, useState } from "react";
 import useUserStore from "../stateManagement/userInfoStore";
-import { useNavigate } from 'react-router-dom';
-
-
+import { useNavigate } from "react-router-dom";
+import MyLikes from "./myLikes";
 
 const MyProfile = () => {
-    const [profile, setProfile] = useState(null);
-    const [error, setError] = useState(null);
-    const {username} = useUserStore();
-    const navigate = useNavigate();
+  const [profile, setProfile] = useState(null);
+  const [error, setError] = useState(null);
+  const { username } = useUserStore();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        if( !username) {
-            navigate("./Shart-FE")
+  useEffect(() => {
+    if (!username) {
+      navigate("./Shart-FE");
+    }
+
+    const fetchProfile = async () => {
+      try {
+        const res = await fetch(
+          `https://codd.cs.gsu.edu/~zbronola1/SoftwareEngineering/shart/users.php?username=${username}`
+        );
+        if (!res.ok) {
+          throw new Error("Failed to fetch");
         }
+        const data = await res.json();
+        if (data.error) {
+          throw new Error(data.error); // Handle the error returned from PHP
+        }
+        setProfile(data);
+      } catch (error) {
+        setError(error.message);
+        console.error(error);
+      }
+    };
 
-        const fetchProfile = async () => {
-            try {
-                const res = await fetch(`https://codd.cs.gsu.edu/~zbronola1/SoftwareEngineering/shart/users.php?username=${username}`);
-                if (!res.ok) {
-                    throw new Error("Failed to fetch");
-                }
-                const data = await res.json();
-                if (data.error) {
-                    throw new Error(data.error); // Handle the error returned from PHP
-                }
-                setProfile(data);
-            } catch (error) {
-                setError(error.message);
-                console.error(error);
-            }
-        };
+    fetchProfile();
+  }, [username, navigate]);
 
-        fetchProfile();
-    }, [username, navigate]);
+  const handleLogout = () => {
+    useUserStore.getState().logout();
+    navigate("/Shart-FE");
+  };
 
-    const handleLogout = () => {
-        useUserStore.getState().logout();
-        navigate("/Shart-FE");
-    }
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+  if (!profile) {
+    return <div>Loading...</div>;
+  }
 
-    if (error) {
-        return <div>Error: {error}</div>;
-    }
-    if (!profile) {
-        return <div>Loading...</div>;
-    }
-    return (
-        <div>
-            <img src={profile[0].profilePic} alt="Profile" />
-            <h1 >{profile[0].name}</h1>
-            <p>Username: {profile[0].username}</p>
-            <p>Bio: {profile[0].bio}</p>
+  return (
+    <div>
+      <img src={profile[0].profilePic} alt="Profile" />
+      <h1>{profile[0].name}</h1>
+      <p>Username: {profile[0].username}</p>
+      <p>Bio: {profile[0].bio}</p>
 
-            <div><button onClick={() => handleLogout()}>Logout</button></div>
-        </div>
-    );
+      <div>
+        <button onClick={() => handleLogout()}>Logout</button>
+      </div>
+
+      <MyLikes />
+    </div>
+  );
 };
 
 export default MyProfile;
